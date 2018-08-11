d903befb96a7a54df10451e3f74aa0b40c2a9f13
Hai Tran <tuanhai.dev@gmail.com>
2018-08-11 08:14:57 +0700

    Setup project without using Storyboard

---
+++ GroupedChatMessage/ViewController.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+class ChatViewController: UITableViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    title = "Message"
+    navigationController?.navigationBar.prefersLargeTitles = true
+  }
+}
+
+
+
